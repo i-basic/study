4b1b1304d402a8b1f4f428cfa4a5d63518f5859a
i-basic <48933511+i-basic@users.noreply.github.com>
2019-03-26 16:35:31 +0900

    Create showf.php

---
+++ ios/1-Label/01-ExLabel/showf.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+
+<html>
+<head>
+      <meta charset="UTF-8" />
+
+</head>
+
+
+<body>
+<h1> show txt php  php   php   php  file</h1>
+<?php include('news.txt');?>
+</body>
+
+
+</html>
